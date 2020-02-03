eaa7a439322bc4a03afd73a6eb04133c02daeaee
thegreatbel-o <belinda.l.chiu@vanderbilt.edu>
2020-02-03 13:36:55 -0600

    Added .gitignore to ignore node modules. Added expressjs and removed polka for the server.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
+
+const express = require('express');
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = new express();
+
+app.use(sapper.middleware());
+
+app.listen(PORT, () => {
+	console.log('App started and available at http://localhost:3000');
+});
