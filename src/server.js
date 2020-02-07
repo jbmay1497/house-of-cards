391045f0a28ddbba60a8e162ee89517fc0b2eedb
jbmay1497 <jared.b.may@vanderbilt.edu>
2020-02-07 15:55:48 -0600

    server update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import * as sapper from '@sapper/server';
 
-const express = require('express');
+let express = require('express'),
+	path =require("path");
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = new express();
+let app = new express();
 
 app.use(sapper.middleware());
+app.use(express.static(path.join(__dirname, "../static")));
 
 app.listen(PORT, () => {
-	console.log('App started and available at http://localhost:3000');
+	console.log(`App started and available at http://localhost:${PORT}`);
 });
