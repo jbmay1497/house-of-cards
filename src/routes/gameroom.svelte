a101d2b3496c3d1463a68052a74c18241418c940
jbmay1497 <jared.b.may@vanderbilt.edu>
2020-02-19 12:20:21 -0600

    added sirv

--- src/routes/gameroom.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-    <title>Gameroom</title>
-</svelte:head>
-
-<h1>Game Room</h1>
-<form>
-    Name: <label><input type="text" name="name"></label><br>
-    <input type="submit" value="Submit">
-</form>
