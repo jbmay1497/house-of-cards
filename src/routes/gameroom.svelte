c76971ee86fd174ca6e3eca48b9f2122bb6d97b0
claraho <claraho34516@gmail.com>
2020-02-19 12:23:12 -0600

    add dropdown

--- src/routes/gameroom.svelte
+++ src/routes/gameroom.svelte
@@ -2,8 +2,70 @@
     <title>Gameroom</title>
 </svelte:head>
 
+<style>
+    .dropbtn {
+        background-color: #4CAF50;
+        color: white;
+        padding: 16px;
+        font-size: 16px;
+        border: none;
+    }
+
+    /* The container <div> - needed to position the dropdown content */
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    /* Dropdown Content (Hidden by Default) */
+    .dropdown-content {
+        display: none;
+        background-color: #f1f1f1;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    /* Links inside the dropdown */
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    /* Change color of dropdown links on hover */
+    .dropdown-content a:hover {background-color: #ddd;}
+
+    /* Show the dropdown menu on hover */
+    .dropdown:hover .dropdown-content {display: block;}
+
+    /* Change the background color of the dropdown button when the dropdown content is shown */
+    .dropdown:hover .dropbtn {background-color: #3e8e41;}
+</style>
+
+<script>
+    // function jumpto(x){
+    //     if (document.form1.jumpmenu.value != "null") {
+    //         document.location.href = x
+    //     }
+    // }
+
+    //let src = "../../static/images/face_down.jpg";
+</script>
+
 <h1>Game Room</h1>
-<form>
-    Name: <label><input type="text" name="name"></label><br>
-    <input type="submit" value="Submit">
-</form>
+
+<div class="dropdown">
+    <button class="dropbtn">Existing Games</button>
+    <div class="dropdown-content">
+        <a href="#">Old Maid</a>
+        <a href="#">War</a>
+        <a href="#">Solitaire</a>
+    </div>
+
+<!--    <figure>-->
+<!--        <img alt='Borat' src='./static/images/2_of_clubs.png'>-->
+<!--    </figure>-->
+<!--    <img src = '../../static/ace_of_spades2.png' alt="face down card"/>-->
+</div>
