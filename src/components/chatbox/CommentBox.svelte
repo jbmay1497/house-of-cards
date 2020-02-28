0dc76513e7f1549477e8b6315275b71c0aecc93e
jbmay1497 <jared.b.may@vanderbilt.edu>
2020-02-28 11:23:47 -0600

    chat

---
+++ src/components/chatbox/CommentBox.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Comment from "./Comment.svelte";
+</script>
+
+<style>
+      #comments {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      }
+      #comments li {
+      padding: 5px 10px;
+      }
+      #comments li:nth-child(odd) {
+      background: #eee;
+      }
+</style>
+<ul id = "comments">
+    <Comment message="This is the chatbox!"/>
+    <Comment message="Messages will appear here pls"/>
+</ul>
--- src/components/chatbox/_CommentBox.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Comment from "./_Comment.svelte";
-</script>
-
-<ul>
-    <Comment message="This is the chatbox!"/>
-    <Comment message="Messages will appear here pls"/>
-</ul>
