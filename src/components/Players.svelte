852de8131ace1260ee5d6b220c64c2e175e18c12
jbmay1497 <jared.b.may@vanderbilt.edu>
2020-02-27 20:20:15 -0600

    added players component

---
+++ src/components/Players.svelte
@@ -0,0 +1,44 @@
+<style>
+ .playerPanel{
+    margin: 0.25rem;
+   min-width: 15em;
+   border: 1px solid #dedede;
+ }
+
+ .playerHeader{
+    margin-block-start: 0.25em;
+    margin-block-end: 0.25em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-left: 1rem;
+  }
+
+  .playerList{
+    list-style-type: none;
+    padding-left: 0;
+  }
+
+  .playerItem{
+    color: grey;
+    padding-right: 1rem;
+    padding-left: 20px;
+  }
+
+</style>
+
+<script>
+export let usernames
+</script>
+
+<div class = "playerPanel">
+    <div class = "playerHeader">Players:</div>
+    <div class = "playerList">
+        {#each usernames as username, i}
+            <li class = playerItem>
+                {i+1} : {username}
+            </li>
+        {/each}
+    </div>
+</div>
+
+
--- src/routes/lobbies/[lobby_id].svelte
+++ src/routes/lobbies/[lobby_id].svelte
@@ -2,93 +2,6 @@
     <title>lobby</title>
 </svelte:head>
 
-<script context="module">
-
-    //these keep track of new users who join
-    //allows us to update the lobbys for other users in the lobby
-    //when a new user joins, without having to fetch from the db.
-    //They must be stores so they can be reactive within the component
-    import { writable } from 'svelte/store';
-    const new_count = writable(0);
-    const new_usernames = writable([]);
-
-    //called when a new user enters the lobby
-    export function updateLobby (data){
-        console.log(`Updating lobby`);
-         new_count.set(data.count);
-         new_usernames.set(data.usernames);
-     }
-
-	export async function preload({params}, session) {
-        //checks if the user enters the lobbies through the /enter route,
-        //or through the lobbys url
-	    let joined = !!(session.lobby_id && session.username);
-
-	    //checks if user is in a different lobby, then redirects them there
-		if (session.lobby_id && session.lobby_id !== params.lobby_id) {
-		  return this.redirect(302, `lobbies/${session.lobby_id}`);
-		}
-
-		//fetching lobbies data
-		const res = await this.fetch(`api/lobby/${params.lobby_id}`, {
-		    method: 'GET',
-		    credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-             }
-		});
-
-		const data = await res.json();
-		if (res.status === 200) {
-            data.joined = joined;
-            data.username = joined ? session.username : "";
-            return { lobby: data };
-        } else {
-            this.redirect(302, ``)
-        }
-
-	}
-</script>
-
-<script>
-     import Modal from '../../components/Modal.svelte';
-
-     //allows us to retrieve the lobbies from the module context
-
-     export let lobby;
-
-     let lobby_id = lobby._id;
-     let usernames = lobby.usernames;
-     let count = lobby.count;
-     let joined = lobby.joined;
-     let username = lobby.username;
-
-     //set initial values for new_count and new_usernames,
-     //so they are not set to default values
-     $new_count = count;
-     $new_usernames = usernames;
-
-     //updates count and usernames whenever new_count and new_username changes
-     $: if ($new_count !== count){
-         count = $new_count;
-     }
-     $: if ($new_usernames.length !== usernames.length){
-         usernames = $new_usernames;
-     }
-
-     //handles joined event from the modal
-     const Joined = event => {
-         joined = event.detail.joined;
-         username = event.detail.username;
-     }
-
-     import {goto} from "@sapper/app"
-
-     function handleCardClick() {
-         goto("game/custom")
-     }
-</script>
-
 <style>
     .hidden{
     display: none;
@@ -170,6 +83,96 @@
     }
 </style>
 
+<script context="module">
+
+    //these keep track of new users who join
+    //allows us to update the lobbys for other users in the lobby
+    //when a new user joins, without having to fetch from the db.
+    //They must be stores so they can be reactive within the component
+    import { writable } from 'svelte/store';
+    const new_count = writable(0);
+    const new_usernames = writable([]);
+
+    //called when a new user enters the lobby
+    export function updateLobby (data){
+        console.log(`Updating lobby`);
+         new_count.set(data.count);
+         new_usernames.set(data.usernames);
+     }
+
+	export async function preload({params}, session) {
+        //checks if the user enters the lobbies through the /enter route,
+        //or through the lobbys url
+	    let joined = !!(session.lobby_id && session.username);
+
+	    //checks if user is in a different lobby, then redirects them there
+		if (session.lobby_id && session.lobby_id !== params.lobby_id) {
+		  return this.redirect(302, `lobbies/${session.lobby_id}`);
+		}
+
+		//fetching lobbies data
+		const res = await this.fetch(`api/lobby/${params.lobby_id}`, {
+		    method: 'GET',
+		    credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+             }
+		});
+
+		const data = await res.json();
+		if (res.status === 200) {
+            data.joined = joined;
+            data.username = joined ? session.username : "";
+            return { lobby: data };
+        } else {
+            this.redirect(302, ``)
+        }
+
+	}
+</script>
+
+<script>
+     import Modal from '../../components/Modal.svelte';
+     import Players from '../../components/Players.svelte';
+
+     //allows us to retrieve the lobbies from the module context
+
+     export let lobby;
+
+     let lobby_id = lobby._id;
+     let usernames = lobby.usernames;
+     let count = lobby.count;
+     let joined = lobby.joined;
+     let username = lobby.username;
+
+     //set initial values for new_count and new_usernames,
+     //so they are not set to default values
+     $new_count = count;
+     $new_usernames = usernames;
+
+     //updates count and usernames whenever new_count and new_username changes
+     $: if ($new_count !== count){
+         count = $new_count;
+     }
+     $: if ($new_usernames.length !== usernames.length){
+         usernames = $new_usernames;
+     }
+
+     //handles joined event from the modal
+     const Joined = event => {
+         joined = event.detail.joined;
+         username = event.detail.username;
+     }
+
+     import {goto} from "@sapper/app"
+
+     function handleCardClick() {
+         goto("game/custom")
+     }
+</script>
+
+
+
 <body class="container">
     <div class:hidden ={joined}>
         <Modal {lobby_id} on:join = {Joined}/>
@@ -202,13 +205,7 @@
                 <div>Welcome {username}!</div>
                 <div> lobby Code: {lobby_id}</div>
                 <div>Players:</div>
-                <ul>
-                    {#each usernames as username, i}
-                        <li>
-                            {i+1} : {username}
-                        </li>
-                    {/each}
-                </ul>
+                <Players {usernames}/>
             </form>
         </div>
     </div>
